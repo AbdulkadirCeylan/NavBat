c5e37938d8531b378f954b61160d1156ea7edffd
AbdulkadirCeylan <69914932+AbdulkadirCeylan@users.noreply.github.com>
2021-12-29 17:34:31 +0300

    updated

--- Full_Test/Camera_view.py
+++ Full_Test/Camera_view.py
@@ -12,6 +12,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from Yolo_improvement import Tracking
 import cv2
+from scipy import signal
 
 vrep.simxFinish(-1)
 clientID_aux = vrep.simxStart('127.0.0.1',19999,True,True,5000,5)
@@ -20,6 +21,8 @@ if clientID_aux!=-1:
 else:
     print("Not connected to remote API server")
     sys.exit("Could not connect")
+
+
 color = (255, 0, 0)
 tracking = Tracking()
 fake_img = cv2.imread('mushroom.jpeg')
@@ -29,31 +32,40 @@ res,v0=vrep.simxGetObjectHandle(clientID_aux,'Vision_sensor',vrep.simx_opmode_on
 time.sleep(1)
 err, resolution, image = vrep.simxGetVisionSensorImage(clientID_aux, v0, 0, vrep.simx_opmode_oneshot_wait)
 t = time.time()
-plt.show()
 
 while True:
     #getting image
     err, resolution, image = vrep.simxGetVisionSensorImage(clientID_aux, v0, 0, vrep.simx_opmode_oneshot_wait)
     if err == vrep.simx_return_ok:
-        x1 = tracking.x_min
-        y1 = tracking.y_max
-        x2 = tracking.x_max
-        y2 = tracking.y_min
+
         img = np.array(image, dtype = np.uint8)
         img.resize([resolution[0],resolution[0],3])
         Rotated1_image = imutils.rotate(img, angle=180)
         Rotated2_image = cv2.flip(Rotated1_image,1)
         msg_frame = CvBridge().cv2_to_imgmsg(Rotated2_image,"bgr8")
         img_pub.publish(msg_frame)
-
+        x1 = tracking.x_min
+        y1 = tracking.y_max
+        x2 = tracking.x_max
+        y2 = tracking.y_min
         ### Draw bounding box on original image
-        cv2.rectangle(Rotated2_image,(x1, y1), (x2, y2), color, 2)
-        cv2.imshow('frame', Rotated2_image)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        #rate.sleep()
-
+        #cv2.rectangle(Rotated2_image,(x1, y1), (x2, y2), color, 2)
         
+        if tracking.done is True:
+            ### Sub-part of Detected image
+
+            detected_greyscale = cv2.cvtColor(Rotated2_image, cv2.COLOR_BGR2GRAY)
+            sub_img = detected_greyscale[y2:y1,x1:x2]
+
+            #### Correlation ####
+            cor = signal.correlate2d (detected_greyscale,sub_img)
+            #print(cor.shape)
+            cv2.imshow('frame', sub_img)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+
     else:
 
         img = np.array(fake_img, dtype = np.uint8)
